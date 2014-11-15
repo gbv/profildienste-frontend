edd3bee31e140f7bf92921a23612ba619968badf
Luca Keidel <keidel@gbv.de>
2014-11-15 23:11:30 +0100

    Updated gulp file and other build files

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,58 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var minifycss = require('gulp-minify-css');
-var notify = require('gulp-notify');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var path = require('path');
-var prefix = require('gulp-autoprefixer');
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    less = require('gulp-less'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    path = require('path'),
+    notify = require('gulp-notify'),
+    del = require('del');
 
 gulp.task('less', function() {
-    return gulp.src(['less/show.less', 'less/login.less', 'less/theme.less'])
-            .pipe(less({
-                paths: [ path.join(__dirname, 'bower_components', 'bootstrap', 'less') ]
-            }))
-            .pipe(prefix("last 3 version", "> 1%", "ie 8", "ie 7"))
-            .pipe(minifycss())
-            .pipe(gulp.dest('css'))
-            .pipe(notify({"message": "LESS compiled!"}));
+  return gulp.src('src/less/*.less')
+    .pipe(less({
+      paths: [path.join(__dirname, 'bower_components', 'bootstrap', 'less')]
+    }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('css/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css/'))
+    .pipe(notify({ message: 'LESS compiled!'}));
 });
 
-var third_party = [
-    'bower_components/jquery/dist/jquery.min.js',
-    'bower_components/bootstrap/dist/js/bootstrap.min.js',
+var vendor = [
+  'bower_components/angular/angular.js',
+  'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+  'bower_components/angular-route/angular-route.js',
+  'bower_components/jquery/dist/jquery.js',
+  'bower_components/bootstrap/dist/js/bootstrap.js'
 ];
 
 gulp.task('js', function() {
-    return gulp.src(third_party.concat(['js/**/*.js']))
-            .pipe(concat('main.js'))
-            .pipe(uglify())
-            .pipe(gulp.dest('js'))
-            .pipe(notify({"message": "JavaScript compiled!"}));
+  return gulp.src(vendor.concat(['src/js/**/*.js']))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/'))
+    .pipe(notify({ message: 'JavaScript compiled!'}));
 });
 
-gulp.task('build', ['less'], function() {});
+gulp.task('clean', function(callback) {
+    del(['css/', 'js/'], callback);
+});
 
-gulp.task('default', ['build'], function() {
+gulp.task('build', ['clean'] ,function(){
+  gulp.start('less', 'js');
+});
 
-    gulp.watch('less/*.less', ['less']);
+gulp.task('default', ['clean'], function() {
+  gulp.start('build');
 
-    //gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/less/**/*.less', ['less']);
+
+  gulp.watch('src/js/**/*.js', ['js']);
 });
