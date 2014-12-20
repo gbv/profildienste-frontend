901078607596bf2a15068b3900a0e4b3ff31d75a
Luca Keidel <keidel@gbv.de>
2014-12-20 23:27:46 +0100

    Fixed a jQuery related bug
    jQuery and Angular were in wrong order in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -43,9 +43,9 @@ gulp.task('less', function() {
 });
 
 var vendor = [
-  'bower_components/angular/angular.min.js',
-  'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
   'bower_components/jquery/dist/jquery.min.js',
+  'bower_components/angular/angular.min.js',
+  'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js', 
   'bower_components/jquery-ui/jquery-ui.min.js',
   'bower_components/bootstrap/dist/js/bootstrap.min.js',
   'bower_components/ngInfiniteScroll/build/ng-infinite-scroll.min.js'
