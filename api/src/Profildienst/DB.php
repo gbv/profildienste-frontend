35ba5baa53749175e40c7e06ca5f211c2fa1ede3
Luca Keidel <keidel@gbv.de>
2015-02-07 02:01:35 +0100

    Package Profildienst refactored

--- api/src/Profildienst/DB.php
+++ api/src/Profildienst/DB.php
@@ -1,149 +1,145 @@
 <?php
 
-/*
-  Connection to the Database (Singleton)
-*/
-
 namespace Profildienst;
+use Config\Config;
+use Middleware\AuthToken;
 
-class DB{
+/**
+ * Connection to the Database (Singleton)
+ *
+ * Class DB
+ * @package Profildienst
+ */
+class DB {
 
-  // Speichern der Instanz
-  private static $m; 
-  private static $db;
-  private static $opt = array(
-    'safe'    => true,
-    'fsync'   => true,
-    'timeout' => 10000
-  );
- 
-  // Direktes Instanzieren und Clonen verhindern
-  private function __construct() { } 
-  private function __clone() { }
- 
-  private static function init_db() {
-    // Wenn keine Instanz existiert eine neue erstellen
-    if(!isset(self::$m)) {
-      self::$m = new \MongoClient();
-      self::$db = self::$m->selectDB('pd');
-      if(!isset(self::$m)) {
-        throw new \Exception('Connection failed: ' . self::$instance->connect_error);
-      }
-    }
-  }
+    // save the instance
+    private static $m;
+    private static $db;
+    private static $opt = array(
+        'safe' => true,
+        'fsync' => true,
+        'timeout' => 10000
+    );
 
-  public static function getUserData($v, $auth){
-    if (!$c = DB::get(array('_id' => $auth->getID()),'users',array($v => 1),true)){
-      die('Kein Benutzer unter der ID gefunden.');
-    }
-    return isset($c[$v]) ? $c[$v] : NULL;
-  }
-
-  public static function getTitleList($query, $skip, $auth){
-    self::init_db();
-    $collection = new \MongoCollection(self::$db, 'titles');
-    $r=array();
-    $cursor = $collection->find($query);
-    $cnt=$cursor -> count();
-    $settings=self::getUserData('settings', $auth);
-    if($settings['order'] == 'asc'){
-      $o=1;
-    }else{
-      $o=-1;
-    }
-    $sortby=array('erj' => '011@.a' ,'wvn' => '006U.0', 'tit' => '021A.a', 'sgr' => '045G.a', 'dbn' => '006L.0', 'per' => '028A.a');
-
-    if(!is_null($skip)){
-      $lm=\Config\BackendConfig::$pagesize;
-      $cursor = $cursor->skip($lm*$skip);
-      $cursor = $cursor->limit($lm);
-      if ($collection -> count($query, $lm+1 ,$lm*$skip) == $lm+1){
-        $next=true;
-      }else{
-        $next=false;
-      }
-    }
-    
-    $cursor = $cursor->sort(array($sortby[$settings['sortby']] => $o));
-    foreach ($cursor as $doc) {
-      $t = new Title($doc);
-      $id = $t -> getDirectly('_id');
-      $r[$id]=$t;
+    // prevent direct instantiation
+    private function __construct() {
     }
 
-    $ret = array('titlelist' => NULL, 'total' => $cnt);
+    // prevent cloning
+    private function __clone() {
+    }
 
-    if(count($r) > 0){
-      $ret['titlelist'] = new TitleList($r, $auth);
+    private static function init_db() {
+        // create a new instance if we can't use a previous one
+        if (!isset(self::$m)) {
+            self::$m = new \MongoClient();
+            self::$db = self::$m->selectDB('pd');
+            if (!isset(self::$m)) {
+                throw new \Exception('Connection failed');
+            }
+        }
     }
-    
-    return $ret;
-  }
 
-  public static function getTitle($query){
-    self::init_db();
-    $collection = new \MongoCollection(self::$db, 'titles');
-    $c = $collection->findOne($query);
-    return $c ? new Title($c) : $c;
-  }
+    public static function getUserData($v, AuthToken $auth) {
+        if (!$c = DB::get(array('_id' => $auth->getID()), 'users', array($v => 1), true)) {
+            die('Kein Benutzer unter der ID gefunden.');
+        }
+        return isset($c[$v]) ? $c[$v] : NULL;
+    }
 
-  public static function getTitleByID($id){
-    self::init_db();
-    $collection = new \MongoCollection(self::$db, 'titles');
-    $query=array('_id' => $id);
-    $c = $collection->findOne($query);
-    return $c ? new Title($c) : $c;
-  }
+    public static function getTitleList($query, $skip, AuthToken $auth) {
+        self::init_db();
+        $collection = new \MongoCollection(self::$db, 'titles');
+        $r = array();
+        $cursor = $collection->find($query);
+        $cnt = $cursor->count();
+        $settings = self::getUserData('settings', $auth);
+        if ($settings['order'] == 'asc') {
+            $o = 1;
+        } else {
+            $o = -1;
+        }
+        $sortby = array('erj' => '011@.a', 'wvn' => '006U.0', 'tit' => '021A.a', 'sgr' => '045G.a', 'dbn' => '006L.0', 'per' => '028A.a');
 
-  public static function exists($query, $coll){
-    self::init_db();
-    $collection = new \MongoCollection(self::$db, $coll);
-    $c = $collection->findOne($query);
-    return $c ? true : false;
-  }
+        if (!is_null($skip)) {
+            $lm = Config::$pagesize;
+            $cursor = $cursor->skip($lm * $skip);
+            $cursor = $cursor->limit($lm);
+        }
 
-  public static function ins($data, $coll){
-    self::init_db();
-    $collection = new \MongoCollection(self::$db, $coll);
-    try {
-        $res = $collection->insert($data, self::$opt);
-    }
-    catch (\MongoCursorException $mce) {
-        die('Error: '.$mce);
-    }
-    catch (\MongoCursorTimeoutException $mcte) {
-        die('Timeout-Error: '.$mcte);
-    }
-  }
+        $cursor = $cursor->sort(array($sortby[$settings['sortby']] => $o));
+        foreach ($cursor as $doc) {
+            $t = new Title($doc);
+            $id = $t->getDirectly('_id');
+            $r[$id] = $t;
+        }
 
-  public static function get($query, $coll, $fields = array(), $findone = false){
-    self::init_db();
-    $collection = new \MongoCollection(self::$db, $coll);
-    if ($findone){
-      $c = $collection->findOne($query,$fields);
-      return $c;
-    }else{
-      $c = $collection->find($query,$fields);
-      $r=array();
-      foreach ($c as $doc){
-        array_push($r,$doc);
-      }
-      return $r;
-    }
-  }
+        $ret = array('titlelist' => NULL, 'total' => $cnt);
 
-  public static function upd($cond,$data,$coll){
-    self::init_db();
-    $collection = new \MongoCollection(self::$db, $coll);
-    try {
-        $res = $collection->update($cond, $data ,self::$opt);
+        if (count($r) > 0) {
+            $ret['titlelist'] = new TitleList($r, $auth);
+        }
+
+        return $ret;
     }
-    catch (\MongoCursorException $mce) {
-        die('Error: '.$mce);
+
+    public static function getTitle($query) {
+        self::init_db();
+        $collection = new \MongoCollection(self::$db, 'titles');
+        $c = $collection->findOne($query);
+        return $c ? new Title($c) : $c;
     }
-    catch (\MongoCursorTimeoutException $mcte) {
-        die('Timeout-Error: '.$mcte);
+
+    public static function getTitleByID($id) {
+        self::init_db();
+        $collection = new \MongoCollection(self::$db, 'titles');
+        $query = array('_id' => $id);
+        $c = $collection->findOne($query);
+        return $c ? new Title($c) : $c;
     }
-  } 
+
+    public static function exists($query, $coll) {
+        self::init_db();
+        $collection = new \MongoCollection(self::$db, $coll);
+        $c = $collection->findOne($query);
+        return $c ? true : false;
+    }
+
+    public static function ins($data, $coll) {
+        self::init_db();
+        $collection = new \MongoCollection(self::$db, $coll);
+        try {
+            $collection->insert($data, self::$opt);
+        } catch (\MongoCursorException $mce) {
+            die('Error: ' . $mce);
+        }
+    }
+
+    public static function get($query, $coll, $fields = array(), $findone = false) {
+        self::init_db();
+        $collection = new \MongoCollection(self::$db, $coll);
+        if ($findone) {
+            $c = $collection->findOne($query, $fields);
+            return $c;
+        } else {
+            $c = $collection->find($query, $fields);
+            $r = array();
+            foreach ($c as $doc) {
+                array_push($r, $doc);
+            }
+            return $r;
+        }
+    }
+
+    public static function upd($cond, $data, $coll) {
+        self::init_db();
+        $collection = new \MongoCollection(self::$db, $coll);
+        try {
+            $collection->update($cond, $data, self::$opt);
+        } catch (\MongoCursorException $mce) {
+            die('Error: ' . $mce);
+        }
+    }
 }
+
 ?>
--- api/src/Profildienst/TitleList.php
+++ api/src/Profildienst/TitleList.php
@@ -2,91 +2,112 @@
 
 namespace Profildienst;
 
-class TitleList{
-	
-	private $titles;
-	private $count;
+use Middleware\AuthToken;
 
-	public function __construct($titles, $auth){
+/**
+ * Class TitleList
+ * @package Profildienst
+ */
+class TitleList {
 
-		$this -> titles = $titles;
+    /**
+     * @var array Titles
+     */
+    private $titles;
 
-		$data=DB::get(array('_id' => $auth->getID()),'users',array(), true);
-		$watchlists=$data['watchlist'];
-		$this -> watchlists=$data['watchlist'];
-		$cart=$data['cart'];	
-		$done=$data['done'];
-		$defaults=$data['defaults'];
-		$rejected=$data['rejected'];
-		$pending=$data['pending'];
+    /**
+     * @param array $titles Array of titles
+     * @param AuthToken $auth Token
+     */
+    public function __construct(array $titles, AuthToken $auth) {
 
-		$this -> settings = $data['settings'];
+        $this->titles = $titles;
 
-		$this -> def_lft = $defaults['lieft'];
-		$this -> def_bdg = $defaults['budget'];
-		$this -> def_selcode = $defaults['selcode'];
-		$this -> def_ssgnr = $defaults['ssgnr'];
+        $data = DB::get(array('_id' => $auth->getID()), 'users', array(), true);
+        $watchlists = $data['watchlist'];
+        $this->watchlists = $data['watchlist'];
+        $cart = $data['cart'];
+        $done = $data['done'];
+        $defaults = $data['defaults'];
+        $rejected = $data['rejected'];
+        $pending = $data['pending'];
 
-		$this -> bdgs = $data['budgets'];
+        $this->settings = $data['settings'];
 
-		foreach ($watchlists as $watchlist){
-			foreach($watchlist['list'] as $wl){
-				if(isset($this -> titles[$wl])){
-					$this -> titles[$wl] -> inWatchlist();
-					$this -> titles[$wl] -> setWlID($watchlist['id']);
-					$this -> titles[$wl] -> setWlName($watchlist['name']);
-				}
-			}
-		}
+        $this->def_lft = $defaults['lieft'];
+        $this->def_bdg = $defaults['budget'];
+        $this->def_selcode = $defaults['selcode'];
+        $this->def_ssgnr = $defaults['ssgnr'];
 
-		foreach ($cart as $c){
-			if(isset($this -> titles[$c['id']])){
-				$this -> titles[$c['id']] -> inCart();
-				$this -> titles[$c['id']] -> setLft($c['lieft']);
-				$this -> titles[$c['id']] -> setBdg($c['budget']);
-				$this -> titles[$c['id']] -> setSSGNr($c['ssgnr']);
-				$this -> titles[$c['id']] -> setSelcode($c['selcode']);
-				$this -> titles[$c['id']] -> setComment($c['comment']);
-			}
-		}
+        $this->bdgs = $data['budgets'];
+
+        foreach ($watchlists as $watchlist) {
+            foreach ($watchlist['list'] as $wl) {
+                if (isset($this->titles[$wl])) {
+                    $this->titles[$wl]->inWatchlist();
+                    $this->titles[$wl]->setWlID($watchlist['id']);
+                    $this->titles[$wl]->setWlName($watchlist['name']);
+                }
+            }
+        }
+
+        foreach ($cart as $c) {
+            if (isset($this->titles[$c['id']])) {
+                $this->titles[$c['id']]->inCart();
+                $this->titles[$c['id']]->setLft($c['lieft']);
+                $this->titles[$c['id']]->setBdg($c['budget']);
+                $this->titles[$c['id']]->setSSGNr($c['ssgnr']);
+                $this->titles[$c['id']]->setSelcode($c['selcode']);
+                $this->titles[$c['id']]->setComment($c['comment']);
+            }
+        }
 
 
-		foreach ($done as $d){
-			if(isset($this -> titles[$d['id']])){
-				$this -> titles[$d['id']] -> setDone();
-				$this -> titles[$d['id']] -> setLft($d['lieft']);
-				$this -> titles[$d['id']] -> setBdg($d['budget']);
-				$this -> titles[$d['id']] -> setSSGNr($d['ssgnr']);
-				$this -> titles[$d['id']] -> setSelcode($d['selcode']);
-				$this -> titles[$d['id']] -> setComment($d['comment']);
-			}
-		}
+        foreach ($done as $d) {
+            if (isset($this->titles[$d['id']])) {
+                $this->titles[$d['id']]->setDone();
+                $this->titles[$d['id']]->setLft($d['lieft']);
+                $this->titles[$d['id']]->setBdg($d['budget']);
+                $this->titles[$d['id']]->setSSGNr($d['ssgnr']);
+                $this->titles[$d['id']]->setSelcode($d['selcode']);
+                $this->titles[$d['id']]->setComment($d['comment']);
+            }
+        }
 
-		foreach ($pending as $p){
-			if(isset($this -> titles[$p['id']])){
-				$this -> titles[$p['id']] -> setDone();
-				$this -> titles[$p['id']] -> setLft($p['lieft']);
-				$this -> titles[$p['id']] -> setBdg($p['budget']);
-				$this -> titles[$p['id']] -> setSSGNr($p['ssgnr']);
-				$this -> titles[$p['id']] -> setSelcode($p['selcode']);
-				$this -> titles[$p['id']] -> setComment($p['comment']);
-			}
-		}
+        foreach ($pending as $p) {
+            if (isset($this->titles[$p['id']])) {
+                $this->titles[$p['id']]->setDone();
+                $this->titles[$p['id']]->setLft($p['lieft']);
+                $this->titles[$p['id']]->setBdg($p['budget']);
+                $this->titles[$p['id']]->setSSGNr($p['ssgnr']);
+                $this->titles[$p['id']]->setSelcode($p['selcode']);
+                $this->titles[$p['id']]->setComment($p['comment']);
+            }
+        }
 
-		foreach ($rejected as $rj){
-			if(isset($this -> titles[$rj])){
-				$this -> titles[$rj] -> setRejected();
-			}
-		}
-	}
+        foreach ($rejected as $rj) {
+            if (isset($this->titles[$rj])) {
+                $this->titles[$rj]->setRejected();
+            }
+        }
+    }
 
-	public function getTitles(){
-		return $this -> titles;
-	}
+    /**
+     * Getter for the titles
+     *
+     * @return array Titles
+     */
+    public function getTitles() {
+        return $this->titles;
+    }
 
-	public function getCount(){
-		return count($this -> titles);
-	}
+    /**
+     * Number of titles in the list
+     *
+     * @return int number of titles
+     */
+    public function getCount() {
+        return count($this->titles);
+    }
 }
-
 ?>
