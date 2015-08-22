0531a531ff681b14e824cbb0ab7f04c57bb0ea64
Luca Keidel <keidel@gbv.de>
2015-08-22 11:49:24 +0200

    Fixed indent and height for price info

--- web/src/js/app/shared/optionbar/optionController.js
+++ web/src/js/app/shared/optionbar/optionController.js
@@ -1,95 +1,95 @@
-pdApp.controller('OptionController', ['$scope', 'SettingsService', 'ConfigService', '$q', 'SelectService', 'Notification', function($scope, SettingsService, ConfigService, $q, SelectService, Notification) {
+pdApp.controller('OptionController', ['$scope', 'SettingsService', 'ConfigService', '$q', 'SelectService', 'Notification', function ($scope, SettingsService, ConfigService, $q, SelectService, Notification) {
 
-  this.selectAll = function(){
-    if($scope.entries === undefined){
-      return;
-    }
-
-    if(SelectService.getSelected().length === $scope.entries.items.length){
-      SelectService.deselectAll();
-    }else{
-      SelectService.selectAll();
-    }
-  }
-
-  var p =  $q.all([SettingsService.getSortby(), SettingsService.getOrder(), SettingsService.getSelOptions(), ConfigService.getConfig(), ConfigService.getEntries()]);
-
-  p.then(function(data){
-
-    $scope.sortby = data[0].sortby;
-    $scope.order = data[1].order;
-
-    $scope.selected_sorter_key = data[2].sort;
-    for(var i=0; i < data[0].sortby.length; i++){
-      if(data[0].sortby[i].key === data[2].sort){
-        $scope.selected_sorter = data[0].sortby[i].value;
-        break;
-      }
-    }
-
-    $scope.selected_order_key = data[2].order;
-    for(var i=0; i < data[1].order.length; i++){
-      if(data[1].order[i].key === data[2].order){
-        $scope.selected_order = data[1].order[i].value;
-        break;
-      }
-    }
-
-    $scope.showSelectAll = data[3].config.rejectPossible;
-
-    $scope.entries = data[4].entries;
-
-
-  }, function(reason){
-    Notification.error(reason);
-  });
-
-  this.setSorter = function (sorter){
-
-    if(sorter === $scope.selected_sorter_key){
-      return;
-    }
-
-    SettingsService.changeSetting('sortby', sorter).then(function(data){
-
-      $scope.selected_sorter_key = data.value;
-      for(var i=0; i < $scope.sortby.length; i++){
-        if($scope.sortby[i].key === data.value){
-          $scope.selected_sorter = $scope.sortby[i].value;
-          break;
+    this.selectAll = function () {
+        if ($scope.entries === undefined) {
+            return;
         }
-      }
 
-      $scope.entries.reset();
-
-    }, function(reason){
-      Notification.error(reason);
-    });
-    
-  }
-
-  this.setOrder = function (order){
-    
-    if(order === $scope.selected_order_key){
-      return;
+        if (SelectService.getSelected().length === $scope.entries.items.length) {
+            SelectService.deselectAll();
+        } else {
+            SelectService.selectAll();
+        }
     }
 
-    SettingsService.changeSetting('order', order).then(function(data){
+    var p = $q.all([SettingsService.getSortby(), SettingsService.getOrder(), SettingsService.getSelOptions(), ConfigService.getConfig(), ConfigService.getEntries()]);
 
-      $scope.selected_order_key = data.value;
-      for(var i=0; i < $scope.order.length; i++){
-        if($scope.order[i].key === data.value){
-          $scope.selected_order = $scope.order[i].value;
-          break;
+    p.then(function (data) {
+
+        $scope.sortby = data[0].sortby;
+        $scope.order = data[1].order;
+
+        $scope.selected_sorter_key = data[2].sort;
+        for (var i = 0; i < data[0].sortby.length; i++) {
+            if (data[0].sortby[i].key === data[2].sort) {
+                $scope.selected_sorter = data[0].sortby[i].value;
+                break;
+            }
         }
-      }
 
-      $scope.entries.reset();
+        $scope.selected_order_key = data[2].order;
+        for (var i = 0; i < data[1].order.length; i++) {
+            if (data[1].order[i].key === data[2].order) {
+                $scope.selected_order = data[1].order[i].value;
+                break;
+            }
+        }
 
-    }, function(reason){
-      Notification.error(reason);
+        $scope.showSelectAll = data[3].config.rejectPossible;
+
+        $scope.entries = data[4].entries;
+
+
+    }, function (reason) {
+        Notification.error(reason);
     });
-    
-  };
+
+    this.setSorter = function (sorter) {
+
+        if (sorter === $scope.selected_sorter_key) {
+            return;
+        }
+
+        SettingsService.changeSetting('sortby', sorter).then(function (data) {
+
+            $scope.selected_sorter_key = data.value;
+            for (var i = 0; i < $scope.sortby.length; i++) {
+                if ($scope.sortby[i].key === data.value) {
+                    $scope.selected_sorter = $scope.sortby[i].value;
+                    break;
+                }
+            }
+
+            $scope.entries.reset();
+
+        }, function (reason) {
+            Notification.error(reason);
+        });
+
+    }
+
+    this.setOrder = function (order) {
+
+        if (order === $scope.selected_order_key) {
+            return;
+        }
+
+        SettingsService.changeSetting('order', order).then(function (data) {
+
+            $scope.selected_order_key = data.value;
+            for (var i = 0; i < $scope.order.length; i++) {
+                if ($scope.order[i].key === data.value) {
+                    $scope.selected_order = $scope.order[i].value;
+                    break;
+                }
+            }
+
+            $scope.entries.reset();
+
+        }, function (reason) {
+            Notification.error(reason);
+        });
+
+    };
 
 }]);
--- web/src/js/app/shared/select/selectService.js
+++ web/src/js/app/shared/select/selectService.js
@@ -1,72 +1,72 @@
-pdApp.service('SelectService', ['$rootScope', 'RejectService', 'ConfigService', 'Notification', function($rootScope, RejectService, ConfigService, Notification){
-  var selected = [];
+pdApp.service('SelectService', ['$rootScope', 'RejectService', 'ConfigService', 'Notification', function ($rootScope, RejectService, ConfigService, Notification) {
+    var selected = [];
 
-  ConfigService.getConfig().then(function(data){
-    this.config = data.config;
-  }.bind(this));
+    ConfigService.getConfig().then(function (data) {
+        this.config = data.config;
+    }.bind(this));
 
-  ConfigService.getEntries().then(function(data){
-    this.entries = data.entries;
-  }.bind(this));
+    ConfigService.getEntries().then(function (data) {
+        this.entries = data.entries;
+    }.bind(this));
 
-  this.getSelected = function(){
-    return selected;
-  };
+    this.getSelected = function () {
+        return selected;
+    };
 
-  this.selectAll = function(){
-    $rootScope.$broadcast('selectAll');
-  };
+    this.selectAll = function () {
+        $rootScope.$broadcast('selectAll');
+    };
 
-  this.deselectAll = function(){
-    var i;
-    for(i=0; i < selected.length; i++){
-      selected[i].status.selected = false;
-    }
-
-    selected = []; 
-
-    if(i > 0){
-      $rootScope.$broadcast('itemDeselected');
-    }
-  };
-
-  this.select = function(item){
-    if(selected.indexOf(item) >= 0){
-      return;
-    }
-    selected.push(item);
-    item.status.selected = true;
-    $rootScope.$broadcast('itemSelected');
-  };
-
-  this.deselect = function(item){
-    var ind = selected.indexOf(item);
-    if(ind < 0){
-      return;
-    }
-
-    selected.splice(ind, 1);
-    item.status.selected = false;
-
-    $rootScope.$broadcast('itemDeselected');
-  };
-
-  this.rejectAll = function(){
-    RejectService.addMultRejected(selected).then(function(data){
-
-      for(var i = 0; i < selected.length; i++){
-        selected[i].status.rejected = true;
-
-        if(this.config.hideRejected){
-          this.entries.removeItem(selected[i]);
+    this.deselectAll = function () {
+        var i;
+        for (i = 0; i < selected.length; i++) {
+            selected[i].status.selected = false;
         }
-      }
 
-      this.deselectAll();
+        selected = [];
 
-    }.bind(this), function(reason){
-      Notification.error(reason);
-    });
-  }.bind(this);
+        if (i > 0) {
+            $rootScope.$broadcast('itemDeselected');
+        }
+    };
+
+    this.select = function (item) {
+        if (selected.indexOf(item) >= 0) {
+            return;
+        }
+        selected.push(item);
+        item.status.selected = true;
+        $rootScope.$broadcast('itemSelected');
+    };
+
+    this.deselect = function (item) {
+        var ind = selected.indexOf(item);
+        if (ind < 0) {
+            return;
+        }
+
+        selected.splice(ind, 1);
+        item.status.selected = false;
+
+        $rootScope.$broadcast('itemDeselected');
+    };
+
+    this.rejectAll = function () {
+        RejectService.addMultRejected(selected).then(function (data) {
+
+            for (var i = 0; i < selected.length; i++) {
+                selected[i].status.rejected = true;
+
+                if (this.config.hideRejected) {
+                    this.entries.removeItem(selected[i]);
+                }
+            }
+
+            this.deselectAll();
+
+        }.bind(this), function (reason) {
+            Notification.error(reason);
+        });
+    }.bind(this);
 
 }]);
