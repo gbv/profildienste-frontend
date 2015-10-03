846336939e6c5d729549632fbf43465eeff3c8ec
Luca Keidel <keidel@gbv.de>
2015-10-04 01:03:50 +0200

    Fixed orderlist bug

--- web/src/js/app/shared/apiServices/orderService.js
+++ web/src/js/app/shared/apiServices/orderService.js
@@ -1,24 +1,27 @@
 pdApp.service('OrderService', ['$http', '$q', 'LoginService', function($http, $q, LoginService) {
 
-  var defOrder = $q.defer();
+  var defOrder;
 
-  LoginService.whenLoggedIn().then(function(data){
-    $http.get('/api/user/orderlist').success(function(json){
-      if(!json.success){
-        defOrder.reject(json.message);
-      }else{
+  var loadOrderlist = function () {
+    defOrder = $q.defer();
+    LoginService.whenLoggedIn().then(function (data) {
+      $http.get('/api/user/orderlist').success(function (json) {
+        if (!json.success) {
+          defOrder.reject(json.message);
+        } else {
+          defOrder.resolve({
+            orderlist: json.data.orderlist
+          });
 
-        defOrder.resolve({
-          orderlist: json.data.orderlist
-        });
-
-      }
-    }.bind(this)).error(function(reason){
-      defCart.reject(reason);
-    });
-  }.bind(this));
+        }
+      }.bind(this)).error(function (reason) {
+        defCart.reject(reason);
+      });
+    }.bind(this));
+  };
 
   this.getOrderlist = function(){
+    loadOrderlist();
     return defOrder.promise;
   };
 
