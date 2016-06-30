fbdd6b6f9bb53105d8eb3f8421fda55c2da04f1e
Luca Keidel <keidel@gbv.de>
2016-06-30 14:38:30 +0200

    Fixed and simplified rejectService

--- src/js/app/shared/apiServices/rejectService.js
+++ src/js/app/shared/apiServices/rejectService.js
@@ -1,63 +1,42 @@
 pdApp.service('RejectService', ['$http', '$rootScope', '$q', function ($http, $rootScope, $q) {
 
-  this.addRejected = function (data, view) {
+    this.addRejected = function (data, view) {
 
-    var def = $q.defer();
+        var items = data;
+        if (data.constructor !== Array) {
+            items = [data.id];
+        }
 
-    var items = data;
-    if (data.constructor !== Array) {
-      items = [data.id];
-    }
+        var affected = (view === undefined || view === '') ? items : view;
 
-    var v = (view === undefined) ? '' : view;
+        return $http({
+            method: 'POST',
+            url: '/api/rejected/add',
+            data: $.param({
+                affected: affected
+            }),
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        });
 
-    $http({
-      method: 'POST',
-      url: '/api/reject/add',
-      data: $.param({
-        id: items,
-        view: v
-      }),
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-    }).success(function (json) {
-      if (!json.success) {
-        def.reject(json.errormsg);
-      } else {
-        def.resolve();
-      }
-    }.bind(this));
+    };
 
-    return def.promise;
-  };
+    this.removeRejected = function (data, view) {
 
-  this.removeRejected = function (data, view) {
+        var items = data;
+        if (data.constructor !== Array) {
+            items = [data.id];
+        }
 
-    var def = $q.defer();
+        var affected = (view === undefined || view === '') ? items : view;
 
-    var items = data;
-    if (data.constructor !== Array) {
-      items = [data.id];
-    }
-
-    var v = (view === undefined) ? '' : view;
-
-    $http({
-      method: 'POST',
-      url: '/api/reject/remove',
-      data: $.param({
-        id: items,
-        view: v
-      }),
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-    }).success(function (json) {
-      if (!json.success) {
-        def.reject(json.errormsg);
-      } else {
-        def.resolve();
-      }
-    }.bind(this));
-
-    return def.promise;
-  };
+        return $http({
+            method: 'POST',
+            url: '/api/rejected/remove',
+            data: $.param({
+                affected: affected
+            }),
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        });
+    };
 
 }]);
